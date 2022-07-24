0aa3593714d9e8f498f6bc5818373911cd44bfa0
Sebastian B <sebbarry@comcast.net>
2022-07-24 15:06:52 -0600

    Update HW3

---
+++ hw3.py
@@ -0,0 +1,13 @@
+# Homework 3 - Powershell and Bash Scripting for Security Administration
+# CSCI 4742
+# Sebastian Barry
+
+startup = {
+  "[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders] Startup=\"C:\windows\start menu\programs\startup\"",
+  "[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders] Startup=\"C:\windows\start menu\programs\startup\""}
+
+def main():
+  return 1
+
+
+main()
